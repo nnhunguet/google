62ca7c4a9fb9ee99b5f6080aa0d19a787dd38ec0
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-05-30 14:25:00 +0700

    Google

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
+import {ReactComponent as Search} from './icons/search.svg'
+import {ReactComponent as Mic} from './icons/mic.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElement = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElement.current.focus();
+  }
+  render() {
+    return(
+      <div className='App'>  
+        <div className='header'>
+          <p>Gmail</p>
+          <p>Hình ảnh</p>
+        </div>
+        <div className='container'>
+          <div>
+            <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' alt='photo'/>
+          </div>
+          <div className='wrap-input'>
+            <Search/>
+            <input type='text' ref={this.inputElement}/>
+            <img src='https://www.google.com/tia/tia.png'/>
+            <Mic/>
+          </div>
+          <div className='option'>
+            <div><p>Google Search</p></div>
+            <div><p>I'm Feeling Lucky</p></div>
+          </div>
+          <div>Nguyen Nghia Hung - UET</div>
+        </div>
+        <div className='footer'>
+          <div className='wrap-ul'>
+            <ul className='d-flex'>
+              <li>Advertising</li>
+              <li>Business</li>
+              <li>About</li>
+              <li>How search work</li>
+            </ul>
+            <ul className='d-flex'>
+              <li>Privacy</li>
+              <li>Term</li>
+              <li>Settings</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
